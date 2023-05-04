c815ebbe0705e2b875cf076c4ed4af234af6b21b
karolllopes <karolinna.lopes@etec.sp.gov.br>
2023-05-04 07:54:10 -0300

    dice_atualizado

---
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2018 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage : ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Get the Button view from the layout and assign a click
+        // listener to it.
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener { rollDice() }
+        diceImage = findViewById(R.id.dice_image)
+    }
+
+    private fun rollDice() {
+        diceImage.setImageResource(getRandomDiceImage())
+    }
+
+    private fun getRandomDiceImage() : Int {
+        val randomInt = (1..6).random()
+
+        return when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        val txtResultado: TextView= findViewById(R.id.txtNumberOfSort)
+        val edtNumber: EditText = findViewById(R.id.edtNumber)
+        val number: Int = edtNumber.text.toString().toInt()
+        if(randomInt == number) {
+            txtResultado.setText("Você ganhou!!")
+        }else
+            txtResultado.setText("Você perdeu!!")
+
+    }
+}
